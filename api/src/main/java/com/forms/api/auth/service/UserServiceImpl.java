7d07d4b8f6a011d8da15313a97221b0769764ca6
rtg <rtg1539@naver.com>
2024-02-13 18:23:18 +0900

    refactor: security config 수정

--- api/src/main/java/com/forms/api/auth/service/UserServiceImpl.java
+++ api/src/main/java/com/forms/api/auth/service/UserServiceImpl.java
@@ -3,26 +3,26 @@ package com.forms.api.auth.service;
 import com.forms.api.auth.infrastructure.UserPrincipal;
 import com.forms.api.member.domain.Member;
 import com.forms.api.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
     private final MemberRepository memberRepository;
 
-    public CustomUserDetailsService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     @Override
-    public UserDetails loadUserByUsername(String email) {
-        Member member = memberRepository.findByEmail(email)
-            .orElseThrow(() ->
-                new UsernameNotFoundException("사용자를 찾을 수 없습니다. email: " + email)
-            );
-
-        return UserPrincipal.create(member);
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                Member member = memberRepository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                return new UserPrincipal(member);
+            }
+        };
     }
 }
